d7aab0901987d4d20bf91272380b9ae76f756691
Eduard von Briesen <eduard.von-briesen@adesso.de>
2022-11-15 15:24:19 +0100

    added basic scatterplot example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ScatterplotBasicDemo } from "./examples/scatterplot/ScatterplotBasicDemo";
+
 function App() {
   return (
     <div>
       Cool charts go here
+      <h1>Scatterplot </h1>
+      <ScatterplotBasicDemo />
     </div>
   );
 }
