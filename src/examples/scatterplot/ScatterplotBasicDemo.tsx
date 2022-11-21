0c467634655a4174385563e5d6effbae9284bae9
Eduard von Briesen <eduard.von-briesen@adesso.de>
2022-11-21 13:26:28 +0100

    removed scatterplot

--- src/examples/scatterplot/ScatterplotBasicDemo.tsx
+++
@@ -1,6 +0,0 @@
-import { data } from "./data";
-import { Scatterplot } from "./Scatterplot";
-
-export const ScatterplotBasicDemo = ({ width = 700, height = 400 }) => (
-  <Scatterplot data={data} width={width} height={height} />
-);
