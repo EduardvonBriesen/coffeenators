a31d690ec119ce6b01196ecd7eac8507d18e597f
Eduard von Briesen <eduard.von-briesen@adesso.de>
2022-11-30 12:17:56 +0100

    added data points to regions

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -3,6 +3,7 @@ import { setMapProjection } from "../../helpers/setMapProjection";
 import { useMapTools } from "../../hooks/useMapTools";
 import Region from "./Region";
 import styled from "styled-components";
+import coffeeData from "../../data/combined_data.json";
 
 const MapContainer = styled.svg`
   height: 50rem;
@@ -13,6 +14,7 @@ export default function Map() {
   // step 1: load geoJSON and create tooltip
   const { mapData } = useMapTools();
 
+
   // render map only when map data is fully loaded
   if (!mapData.loading) {
     // step 2: render the regions
@@ -20,13 +22,14 @@ export default function Map() {
     const path = d3.geoPath().projection(setMapProjection(mapData.data));
     // for each geoJSON coordinate, compute and pass in the equivalent svg path
     const countries = mapData.data.features.map((data: any) => {
-      console.log(data);
       const region_name = data.properties["NAME"];
+      const region_data = coffeeData.filter((d) => d.Region === region_name);
       return (
         <Region
           key={data.properties.FID}
           path={path(data) || ""}
           tooltipData={region_name}
+          data={region_data}
         />
       );
     });
--- src/components/map/Region.tsx
+++ src/components/map/Region.tsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import {
+  handleMouseOver,
+  handleMouseOut,
+  handleMouseMove,
+} from "../../helpers/handleTooltip";
 
 const RegionContainer = styled.path`
   fill: ${(props) => props.theme.colors.secondary};
@@ -13,20 +18,34 @@ const RegionContainer = styled.path`
 interface Props {
   path: string;
   tooltipData: string;
+  data?: any;
 }
 
-export default function Region({ path, tooltipData }: Props) {
+export default function Region({ path, tooltipData, data }: Props) {
+  const year = 2019;
+  const dataType = "Durchschnittliches Volumen pro Kopf";
+  const dataName = "Kaffee";
+
+  const dataPoint = data.filter(
+    (d: any) => d.Diagram === dataType && d.Name === dataName
+  )[0];
+
+  const value = dataPoint ? dataPoint[year] : undefined;
+
+  console.log(value);
+
   //each path defines the shape of a region in the map
   return (
     <RegionContainer
       d={path}
-      // onMouseOver={() => {
-      //   handleMouseOver(tooltipData);
-      // }}
-      // onMouseOut={handleMouseOut}
-      // onMouseMove={(event) => {
-      //   handleMouseMove(event);
-      // }}
+      opacity={value ? 1 : 0.5}
+      onMouseOver={() => {
+        handleMouseOver(value);
+      }}
+      onMouseOut={handleMouseOut}
+      onMouseMove={(event) => {
+        handleMouseMove(event);
+      }}
     />
   );
 }
