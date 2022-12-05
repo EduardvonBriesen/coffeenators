d6f5a3d00598766dbc910b18d853be993ca43cb3
Elisabeth Mayer <e.mayer@lrz.uni-muenchen.de>
2022-12-05 17:41:28 +0100

    first style attempt, all black with blue thumb

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -8,6 +8,50 @@ function changeDate() {
   document.getElementById("output").value = CurrentDate;
 }
 
+const Text = styled.p`
+  column-count: 1;
+  text-align: center;
+  max-width: 750px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+`;
+
+const Label = styled.label`
+  text-align: center;
+  max-width: 50px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 0px;
+`;
+const Range = styled.input`
+width: 500px;
+margin: 0px 20px;
+background: transparent; 
+background-color: transparent;
+
+&:focus{
+    outline: none; 
+}
+
+&::-webkit-slider-thumb {
+  box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
+  height: 20px;
+  width: 39px;
+  background: #000000;
+  border: 1px solid #000000;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -3.6px;
+}
+
+&::-webkit-slider-runnable-track {
+  background: #000000;
+  width: 100%;
+  height: 8.4px;
+  cursor: pointer;
+}
+`;
 
 
 function Slider() {
@@ -16,13 +60,13 @@ function Slider() {
 
 
           <div>
-          <label for="startDate">2018</label>
-          <input type="range" id="sliderDate" name="sliderDateName" min="2018" max="2022" list="steplist" onInput={changeDate}/>
+          <Label for="startDate">2018</Label>
+          <Range type="range" id="sliderDate" name="sliderDateName" min="2018" max="2022" list="steplist" onInput={changeDate}/>
           
-          <label for="EndDate">2022</label>
+          <Label for="EndDate">2022</Label>
 
           </div>
-          <p id="result">Average consumption in Europe in <output id="output" value="2018"/></p>
+          <Text id="result">Average consumption in Europe in <output id="output" value="2018"/></Text>
 
 
           <datalist id="steplist">
